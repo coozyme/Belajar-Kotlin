f337b07e6149fbfb0c4c0dd6de467811231007b2
arysetya <programmeranak@gmail.com>
2020-08-08 16:54:06 +0700

    Recursive Function

---
+++ src/RecursiveFunction.kt
@@ -0,0 +1,23 @@
+fun main() {
+
+    //Looping Biasa dengan recursive biasa
+    fun factorialLoop(value: Int): Int {
+        var result = 1
+        for (i in value downTo 1) {
+            result*=i
+        }
+        return result
+    }
+
+    println(factorialLoop(10))
+
+    //Looping Dengan recursive function
+    fun factorialRecursive(value: Int): Int {
+        return when (value) {
+            1 -> 1
+            else -> value * factorialRecursive(value -1)
+        }
+    }
+
+    println(factorialRecursive(10))
+}
