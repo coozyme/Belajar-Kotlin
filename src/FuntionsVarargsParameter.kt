a7ab0a940240bb1eab119ec1f4db667d4daff8bb
arysetya <programmeranak@gmail.com>
2020-08-08 14:32:27 +0700

    FuntionVarargsParameter

---
+++ src/FuntionsVarargsParameter.kt
@@ -0,0 +1,27 @@
+fun hitungTotal1(values: Array<Int>): Int {
+    var total = 0
+
+    for (value in values){
+        total += value
+    }
+    return total;
+}
+//jika menggunakan vararg maka menjadi seperti ini, posisi vararg harus selalu di akhir
+
+fun hitungTotal(name: String, vararg values: Int ): Int {
+    var total = 0
+
+    for (value in values){
+        total += value
+    }
+    return total;
+}
+
+fun main() {
+//    val result = hitungTotal("ary", 10,10,10)
+//    val result = hitungTotal1()
+
+    val values = arrayOf(10,10,10,10)
+    val result =    hitungTotal1(values)
+    println(result)
+}
---
+++ src/SingleExpressionFun.kt
@@ -0,0 +1,7 @@
+fun kaliDua (a: Int) : Int = a * 2
+fun sapa (name: String): Unit = println("Halo $name")
+
+fun main() {
+    println(kaliDua(20))
+    sapa("Ary")
+}
