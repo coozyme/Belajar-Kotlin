85d4eade32f16ae027bd913241ea17ce4a82927f
arysetya <programmeranak@gmail.com>
2020-08-08 16:24:51 +0700

    ReturnIfElse

--- src/ReturnIfElse.kt
+++ src/ReturnIfElse.kt
@@ -1,4 +1,5 @@
 fun main() {
+    //IF
 //    fun sayHello(name: String = ""): String {
 //        if (name == ""){
 //            return "Hello Wolrd"
@@ -7,15 +8,35 @@ fun main() {
 //        }
 //    }
 
-    //kita bisa mempersingkat dengan
 
-    fun sayHello(name: String = ""): String {
-        return if (name == ""){
-             "Hello Wolrd"
-        } else {
-             "Hai $name"
+    //kita bisa mempersingkat dengan dibahah ini
+
+//    fun sayHello(name: String = ""): String {
+//        return if (name == ""){
+//             "Hello Wolrd"
+//        } else {
+//             "Hai $name"
+//        }
+//    }
+
+    //WHEN
+//    fun sayHello(name: String=""):String{
+//        when(name){
+//            "" -> return "Hello When"
+//        else -> return  "Hello $name"
+//        }
+//
+//    }
+
+    //Disin kita bisa mempersingkat syntak dengan
+    fun sayHello(name: String=""):String{
+        return when(name){
+            "" ->  "Hello When"
+            else ->  "Hello $name"
         }
+
     }
+
 
     println (sayHello())
     println (sayHello ("Ary"))
