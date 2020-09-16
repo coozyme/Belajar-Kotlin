4b8deaafb255afe19af9063a073e911435f1d5c9
arysetya <programmeranak@gmail.com>
2020-09-16 19:59:00 +0700

    String

---
+++ src/Basic/String.kt
@@ -0,0 +1,15 @@
+package Basic
+
+fun main(){
+    val FirstName = "Ary"
+    val FullName : String = "Ary Setya Pambudi"
+    val  alamat = """
+        Puri Permai
+        Blok H 11 
+        03/05
+    """.trimIndent()
+
+    println(FirstName)
+    println(FullName)
+    println(alamat)
+}
