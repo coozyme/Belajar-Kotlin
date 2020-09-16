ed8e563557b672f6a9abdbc3e4cdfd67df507768
arysetya <programmeranak@gmail.com>
2020-09-16 21:01:52 +0700

    When Expression

---
+++ src/Basic/WhenExpression.kt
@@ -0,0 +1,14 @@
+package Basic
+
+fun main(){
+    val m = 'a'
+
+    when(m){
+        'a' -> println("mantap")
+        'b' -> println("biasa")
+        'c' -> println("cukup")
+        'd' -> println("dibawah cukup")
+        else -> println("gtau")
+
+    }
+}
