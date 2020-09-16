0a8ddd0a22d29537d22e6908b98ff68320e046f1
arysetya <programmeranak@gmail.com>
2020-09-16 21:41:32 +0700

    While Do Loop

---
+++ src/Basic/WhileDoLoop.kt
@@ -0,0 +1,10 @@
+package Basic
+
+fun main(){
+    var i = 1
+    do{
+        println("ke $i")
+        i++
+    } while ( i<10)
+    println("Berhenti")
+}
--- src/Basic/WhileLoop.kt
+++ src/Basic/WhileLoop.kt
@@ -1,2 +1,10 @@
 package Basic
 
+fun main(){
+    var a = 1
+    while (a < 10 ){
+        println(a)
+        a++
+    }
+    println("Berhenti kosong")
+}
