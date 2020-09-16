fc05bc27de2234efaa92aa9a25397a398abb54ab
arysetya <programmeranak@gmail.com>
2020-09-16 20:56:02 +0700

    If Else Expression

---
+++ src/Basic/IfElseExpression.kt
@@ -0,0 +1,12 @@
+package Basic
+
+fun main(){
+    val nilai = 90
+
+    if(nilai > 60 && nilai < 90){
+        println("Selamat anda lulus")
+    } else  if (nilai >= 90 ) {
+        println("Awesome")
+    } else{ println("maaf anda tidak lulus")}
+
+}
