690c13ac8b52cfd7274c8345ef6da5c844d68972
arysetya <programmeranak@gmail.com>
2020-09-16 20:46:13 +0700

    Convert Type Data

---
+++ src/Basic/ConvertTypeData.kt
@@ -0,0 +1,15 @@
+package Basic
+
+fun main(){
+
+    var angka = 100
+    val a = angka.toString()
+    val b = angka.toDouble()
+    val c = angka.toFloat()
+    val d = angka.toLong()
+
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+}
